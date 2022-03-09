9b14379a3da5587abad443639f526be39c77f891
yrchek <y.chekmaev@mail.ru>
2022-03-09 18:15:53 +0300

    Замена цикла while на for

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ this_list = ['}{}', '{{[(])]}}', '[[{())}]', '{{[()]}}',
              '[([])((([[[]]])))]{()}', '(((([{}]))))']
 
 
-class Stek:
+class Stack:
     def __init__(self, my_string):
         self.my_string = my_string
         self.stek_list = []
@@ -10,48 +10,45 @@ class Stek:
                                '[': ']',
                                '{': '}'}
 
-    def isEmpty(self, array):
-        if len(array) != 0:
+    def isEmpty(self):
+        if len(self.stek_list) != 0:
             return True
         else:
             return False
 
-    def push(self, array, data):
-        array.insert(0, data)
+    def push(self, data):
+        self.stek_list.insert(0, data)
 
-    def pop(self, array):
-        element = array[0]
-        if isinstance(array, str):
-            self.my_string = self.my_string[1:]
-        else:
-            self.stek_list.pop(0)
+    def pop(self):
+        element = self.stek_list[0]
+        self.stek_list.pop(0)
         return element
 
-    def peek(self, array):
-        element = array[0]
+    def peek(self):
+        element = self.stek_list[0]
         return element
 
-    def size(self, array):
-        len_array = len(array)
+    def size(self):
+        len_array = len(self.stek_list)
         return len_array
 
     def checking_balance(self):
-        if self.size(self.my_string) % 2 != 0:
+        if self.size() % 2 != 0:
             return 'Несбалансированно'
-        while self.isEmpty(self.my_string):
-            el = self.pop(self.my_string)
+        for i in range(len(self.my_string)):
+            el = self.my_string[i]
             if el in self.symbol_library:
-                self.push(self.stek_list, self.symbol_library[el])
+                self.push(self.symbol_library[el])
             else:
                 if el not in self.stek_list:
                     return 'Несбалансированно'
                 else:
-                    el_stek = self.peek(self.stek_list)
-                    if el != el_stek:
+                    el_stack = self.peek()
+                    if el != el_stack:
                         return 'Несбалансированно'
                     else:
-                        self.pop(self.stek_list)
-        if self.isEmpty(self.stek_list):
+                        self.pop()
+        if self.isEmpty():
             return 'Несбалансированно'
         else:
             return 'Сбалансированно'
@@ -59,6 +56,6 @@ class Stek:
 
 if __name__ == '__main__':
     for string in this_list:
-        start = Stek(string)
+        start = Stack(string)
         res = start.checking_balance()
         print(res)
